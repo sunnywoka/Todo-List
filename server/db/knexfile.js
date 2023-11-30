a488c745660b81478b0f04697d0edb65008a15d9
Yuekai Wu <yuekaixx@outlook.com>
2023-11-30 15:54:09 +1300

    comment out some code in knexfile

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -23,22 +23,22 @@ export default {
     },
   },
 
-  test: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
-    connection: {
-      filename: ':memory:',
-    },
-    migrations: {
-      directory: Path.join(__dirname, 'migrations'),
-    },
-    seeds: {
-      directory: Path.join(__dirname, 'seeds'),
-    },
-    pool: {
-      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
-    },
-  },
+  // test: {
+  //   client: 'sqlite3',
+  //   useNullAsDefault: true,
+  //   connection: {
+  //     filename: ':memory:',
+  //   },
+  //   migrations: {
+  //     directory: Path.join(__dirname, 'migrations'),
+  //   },
+  //   seeds: {
+  //     directory: Path.join(__dirname, 'seeds'),
+  //   },
+  //   pool: {
+  //     afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
+  //   },
+  // },
 
   production: {
     client: 'pg',
