e3bb5d40d84afe9b9e6776e3e174fbe3b8c48ae9
Yuekai Wu <yuekaixx@outlook.com>
2023-11-30 16:39:13 +1300

    update for deployment

--- server/index.ts
+++ server/index.ts
@@ -2,10 +2,9 @@ import server from './server.ts'
 import dotenv from 'dotenv'
 
 dotenv.config()
-
 const PORT = process.env.PORT || 3000
 
 server.listen(PORT, () => {
   // eslint-disable-next-line no-console
-  console.log('API server listening on port', PORT)
+  console.log('Server listening on port', PORT)
 })
